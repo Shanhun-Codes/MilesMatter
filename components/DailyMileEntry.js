f352668a8659041d70859415f21f7e9324f002e1
Raszero <travisjcelli95@gmail.com>
2024-05-31 13:24:36 -0500

    added geoLocation functionality"

--- components/DailyMileEntry.js
+++ components/DailyMileEntry.js
@@ -1,16 +1,53 @@
-import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const DailyMileEntry = () => {
+const [location, setLocation] = useState()
+const [address, setAddress] = useState()
+
+
+  useEffect(() => {
+    const getPermissions = async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log("Please grant location permissions");
+        return;
+      }
+      let currentLocation = await Location.getCurrentPositionAsync({})
+      setLocation(currentLocation)
+      console.log('Location', currentLocation);
+    };
+    getPermissions();
+  }, []);
+
+  const reverseGeocode = async () => {
+    const reverseGeoCodedAddress = await Location.reverseGeocodeAsync({
+      longitude: location.coords.longitude,
+      latitude: location.coords.latitude
+    })
+    console.log(reverseGeoCodedAddress);
+
+  }
+
+
   return (
     <>
       <View style={style.form}>
         <View style={style.inputContainer}>
-        <TextInput style={style.input} placeholder="Start miles" />
-        <TextInput style={style.input} placeholder="End miles" />
+          <TextInput style={style.input} placeholder="Start miles" />
+          <TextInput style={style.input} placeholder="End miles" />
         </View>
-     <TouchableOpacity style={style.buttonContainer}>
-      <Text style={style.button}>Submit</Text>
-     </TouchableOpacity>
+        <TouchableOpacity style={style.buttonContainer}
+        onPress={reverseGeocode}>
+          <Text style={style.button}>Submit</Text>
+        </TouchableOpacity>
       </View>
     </>
   );
@@ -32,15 +69,15 @@ const style = StyleSheet.create({
     backgroundColor: "coral",
   },
   buttonContainer: {
-    marginTop: 10, 
-    alignItems: 'center',
+    marginTop: 10,
+    alignItems: "center",
   },
   button: {
     fontSize: 20,
     paddingVertical: 10,
-    paddingHorizontal: 20, 
-    backgroundColor: 'aqua',
-  }
+    paddingHorizontal: 20,
+    backgroundColor: "aqua",
+  },
 });
 
 export default DailyMileEntry;
